c6b921d9d8d5b422226e36d5e47b68aa7ad4dbea
Harindu Dilshan ( Kavinda Pitiduwa Gamage) <harindudilshan95@gmail.com>
2021-02-11 23:21:57 -0700

    formatting error

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -5,7 +5,7 @@ exports.notifyChangePassword = (email, name) => {
     let subj = 'Password has been changed';
     let text = 'Dear ' + name + ',' + '<br> Your password has been updated. Please contact the admin for the new password.<br> Thanks, <br> DFIRE Team';
     emailController.sendMail(email, subj, text);
-}
+};
 
 exports.notifyUpdateProfile = (email, name) =>{
     let subj = 'Profile has been changed';
